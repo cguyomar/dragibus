1ef592b5e5bd18efb1f48ae91f371b804901877d
cguyomar <cervin.guyomar@gmail.com>
2022-04-15 11:32:20 +0200

    Fix arg parse

--- src/dragibus/dragibus.py
+++ src/dragibus/dragibus.py
@@ -11,9 +11,14 @@ def main():
     parser.add_argument("--out",dest='out_file')
     parser.add_argument("--fasta",dest='fasta')
 
+    args = parser.parse_args()
+
     # in_file = "small.gtf"
     # fasta = "sus_scrofa.fa"
     # in_file = "novel.gtf"
+    in_file = args.in_file
+    fasta = args.fasta    
+    out_file = args.out_file
 
     genes,transcripts,exons = dragibus.parse_gtf(in_file)
 
